fd884a0afa6be9972436309dfc64bbb1bf352e59
Andrea Freeman <afreeman5566@gmail.com>
2021-12-08 12:55:21 -0600

    added toggle in garden cards for form view. Still needs to be implemented but functionality is created

--- plant_based_frontend/src/Components/GardenCard.js
+++ plant_based_frontend/src/Components/GardenCard.js
@@ -1,16 +1,26 @@
 import { Card } from "react-bootstrap"
 import React, { useState } from "react";
 import EditGardenForm from "./EditGardenForm";
+import {Link} from "react-router-dom"
 
 
 function GardenCard({garden, onDeleteGarden}) {
     const { id} = garden;
+    const [toggle, setToggle] = useState(true);
     const [currentGarden, setCurrentGarden] = useState({});
     const [formData, setFormData] = useState({
         name: "",
         location: "",
         user_id:""
     })
+
+//toggle functionality
+
+function handleToggle(){
+    
+    setToggle(!toggle)
+    console.log(toggle)
+}
 
 //    Delete functionality
     function handleDeleteGarden() {
@@ -34,11 +44,15 @@ function GardenCard({garden, onDeleteGarden}) {
                 <Card.Img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F24%2F2017%2F07%2Fmcclurg_garden_gravel_path_2481501_mcclu20160414-_41a3348-1.jpg&q=85"/>
                 <Card.Title> {garden.name}</Card.Title>
                 <Card.Subtitle>{garden.location}</Card.Subtitle>
-                <button class ="redirect_btn"
-                >View</button>
-                <button class ="redirect_btn">Edit</button>
+                <button class= "redirect_btn"> <Link class="redirect" to="/plants"> View </Link></button> 
+
+                {/* The above link should only show plants for that particular garden */}
+
+                <button onClick={handleToggle} class ="redirect_btn">Edit</button>
                 <button onClick={handleDeleteGarden} class ="redirect_btn">Delete</button>
-                <EditGardenForm garden={garden} id={id}/> // make this conditionally rendered based on whether or not you click the view button
+                <EditGardenForm garden={garden} id={id}/> 
+                {/* // above link should be conditionally rendered based on whether or not you click the view button */}
+                
             </Card.Body>
         </Card>
         </>
