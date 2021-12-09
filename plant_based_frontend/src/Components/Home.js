e534f3664e5c184e4b3f8fb026451c79b6a82dce
James Brownie <jamespbrownie@gmail.com>
2021-12-09 10:31:59 -0600

    homepage update

--- plant_based_frontend/src/Components/Home.js
+++ plant_based_frontend/src/Components/Home.js
@@ -21,13 +21,16 @@ function Home ({setCurrentUser}) {
 
     return (
         <>
+        <div id="home-header">
         <h1>HOME</h1>
         <p>welcome to plant based , the premiere garden management app</p>
-
-        <button id="login_btn" > <Link class="redirect" to="/login"> Login </Link></button>
-        <button id="signup_btn"> <Link class="redirect" to="/signup"> Signup </Link></button>
-        <button id="logout_btn" onClick={handleLogout}> <Link class="redirect" to="/login"> Logout </Link></button>
-        
+        </div>
+        <img id="homepageImg" src="https://media.istockphoto.com/vectors/gardening-tools-and-plants-in-the-garden-vector-id1268196717?k=20&m=1268196717&s=612x612&w=0&h=RBA2SisPRx6OIeouAQ2R7I78eiazDS2gvGPr17mHvy4=" />
+        <div id="login-buttons-div">
+          <button id="login_btn" > <Link class="redirect" to="/login"> Login </Link></button>
+          <button id="signup_btn"> <Link class="redirect" to="/signup"> Signup </Link></button>
+          <button id="logout_btn" onClick={handleLogout}> <Link class="redirect" to="/login"> Logout </Link></button>
+        </div>
         </>
 
     )
