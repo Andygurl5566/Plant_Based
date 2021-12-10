bfabd160c81b6713f5b6eebcdedd94bfd8ac7401
Andrea Freeman <afreeman5566@gmail.com>
2021-12-09 18:03:50 -0600

    finally push before implamenting redux

--- plant_based_frontend/src/Components/GardenForm.js
+++ plant_based_frontend/src/Components/GardenForm.js
@@ -2,9 +2,9 @@ import { Form } from "react-bootstrap"
 import {Link, useNavigate} from 'react-router-dom'
 import React, { useState } from "react";
 
-const GardenForm=({currentUser})=>{
+const GardenForm=({currentUser, garden})=>{
     let navigate = useNavigate()
-    console.log(currentUser)
+    console.log(garden)
     const [currentGarden, setCurrentGarden] = useState({});
     const [formData, setFormData] = useState({
         name: "",
